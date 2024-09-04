641a6ccebf635a732cd181bc934d4b5e4db9ff18
Magistry <Magistrigusti@yandex.ru>
2024-09-05 01:47:18 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { TonConnectUIProvider } from "@tonconnect/ui-react"
+import MainPage from './MainPage';
+
+function App() {
+
+
+  return <TonConnectUIProvider>
+    manifestUrl = "https://ton-connect.github.io/demo-dapp-with-wallet/";
+    <MainPage />
+  </TonConnectUIProvider>
+}
+
+export default App
---
+++ src/MainPage.tsx
@@ -0,0 +1,11 @@
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Welcome to TON Connect React Vite!</h1>
+      <p>This is the main page.</p>
+    </div>
+  )
+}
+
+export default MainPage;
