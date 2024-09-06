9be9be2988104f07d8f1215be16db73e596b8036
Magistry <Magistrigusti@yandex.ru>
2024-09-06 18:54:45 +0300

    0.2a

--- src/SendTx.tsx
+++ src/SendTx.tsx
@@ -1,5 +1,13 @@
+import { useTonConnectModal, useTonWallet } from "@tonconnect/ui-react"
 
 export const SendTx = () => {
+  const wallet = useTonWallet();
+  const { open } = useTonConnectModal();
+
+  if (!wallet) {
+    return <button onClick={ open }>Connect Wallet</button>
+  }
+
   return (
     <>
       <button>Send transaction</button>
