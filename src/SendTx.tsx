9c8094f1bdbc86e135f9f6f0bc4f6339f7b6f2de
Magistry <Magistrigusti@yandex.ru>
2024-09-07 13:33:38 +0300

    0.3

--- src/SendTx.tsx
+++ src/SendTx.tsx
@@ -1,8 +1,29 @@
-import { useTonConnectModal, useTonWallet } from "@tonconnect/ui-react"
+import { useTonConnectModal, useTonWallet, useTonConnectUI } from "@tonconnect/ui-react"
+import { SendTransactionRequest } from "@tonconnect/ui-react";
 
 export const SendTx = () => {
   const wallet = useTonWallet();
   const { open } = useTonConnectModal();
+  const [ tonConnectUI ] = useTonConnectUI();
+
+  const onSendTx = async () => {
+    const tx: SendTransactionRequest = {
+      validUntil: Math.round(Date.now() / 1000) + 60 * 5,
+      network: wallet?.account.chain,
+      from: wallet!.account.address,
+      messages: [
+        {
+          address: 'UQBH1A7LGcrv3_N61S3g_pnLY0bq5I6XUY18oh9wIpCfFq5o',
+          amount: '10000000'
+        }
+      ]
+    }
+
+    const result = await tonConnectUI.sendTransaction(tx);
+    console.log(result.boc);
+  }
+
+  
 
   if (!wallet) {
     return <button onClick={ open }>Connect Wallet</button>
@@ -10,7 +31,7 @@ export const SendTx = () => {
 
   return (
     <>
-      <button>Send transaction</button>
+      <button onClick={onSendTx}>Send transaction</button>
     </>
   )
 }
